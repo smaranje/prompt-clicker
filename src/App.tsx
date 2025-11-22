24ad15c1cc826dc787533f9e887071d3df197109
Smaran <97724824+smaranje@users.noreply.github.com>
2025-11-22 12:08:21 -0500

    Set basename for BrowserRouter in App component

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename="/prompt-clicker">
           <Routes>
             <Route path="/" element={<CategorySelection />} />
             <Route path="/templates/:categoryId" element={<TemplateSelection />} />
